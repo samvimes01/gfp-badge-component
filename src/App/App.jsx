3a1d3da04e137596874c1b55a6ebd7d516217952
Oleksandr Korneiko <oleksandr.korneiko@gmail.com>
2024-05-03 11:26:29 +0300

    feat: display brand badge

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   const sizes = ["xs", "sm", "lg"];
-  const types = ["primary", "error", "success", "warning"];
+  const types = ["primary", "error", "success", "warning", "brand"];
 
   return (
     <div className="grid grid-cols-3 gap-6">
